254abac491ec9a99ac333e014fe1917afa5f2d92
Henry Jooste <henryjooste95@gmail.com>
2022-04-27 09:09:45 +0200

    Documented code

--- Plugins/UTLogger/Source/UTLogger/Private/Core/Log.cpp
+++ Plugins/UTLogger/Source/UTLogger/Private/Core/Log.cpp
@@ -2,30 +2,56 @@
 #include "Misc/FileHelper.h"
 #include "Engine/Engine.h"
 
+/**
+ * @brief Log an error
+ * @param Tag The category of the log entry
+ * @param Text The text to log out
+ */
 void ULog::Error(const FString Tag, const FString Text)
 {
 	LogToFile(ELogType::Error, Tag, Text);
 	LogToScreen(FColor::Red, Tag, Text);
 }
 
+/**
+ * @brief Log a warning
+ * @param Tag The category of the log entry
+ * @param Text The text to log out
+ */
 void ULog::Warning(const FString Tag, const FString Text)
 {
 	LogToFile(ELogType::Warning, Tag, Text);
 	LogToScreen(FColor::Yellow, Tag, Text);
 }
 
+/**
+ * @brief Log info
+ * @param Tag The category of the log entry
+ * @param Text The text to log out
+ */
 void ULog::Info(const FString Tag, const FString Text)
 {
 	LogToFile(ELogType::Info, Tag, Text);
 	LogToScreen(FColor::Blue, Tag, Text);
 }
 
+/**
+ * @brief Log trace information
+ * @param Tag The category of the log entry
+ * @param Text The text to log out
+ */
 void ULog::Trace(const FString Tag, const FString Text)
 {
 	LogToFile(ELogType::Trace, Tag, Text);
 	LogToScreen(FColor::White, Tag, Text);
 }
 
+/**
+ * @brief Log information to a file
+ * @param Type The type of information to log
+ * @param Tag The category of the log entry
+ * @param Text The text to log out
+ */
 void ULog::LogToFile(ELogType Type, const FString Tag, const FString Text)
 {
 #if !PLATFORM_DESKTOP
@@ -46,6 +72,12 @@ void ULog::LogToFile(ELogType Type, const FString Tag, const FString Text)
 	FFileHelper::SaveStringToFile(LogText, *LogFile);
 }
 
+/**
+ * @brief Log information to the screen
+ * @param Color The color used for the log entry
+ * @param Tag The category of the log entry
+ * @param Text The text to log out
+ */
 void ULog::LogToScreen(const FColor Color, const FString Tag, const FString Text)
 {
 #if !UE_EDITOR && !WITH_EDITOR
@@ -61,6 +93,11 @@ void ULog::LogToScreen(const FColor Color, const FString Tag, const FString Text
 	GEngine->AddOnScreenDebugMessage(-1, 5.0f, Color, LogText);
 }
 
+/**
+ * @brief Convert a log type to a user friendly name
+ * @param Type The log type to convert
+ * @return A user friendly name for the log type
+ */
 FString ULog::GetLogTypeText(const ELogType Type)
 {
 	switch (Type)
--- Plugins/UTLogger/Source/UTLogger/Public/Core/Log.h
+++ Plugins/UTLogger/Source/UTLogger/Public/Core/Log.h
@@ -5,26 +5,68 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Log.generated.h"
 
+/**
+ * @brief Log information to file and display on-screen messages
+ */
 UCLASS()
 class UTLOGGER_API ULog final : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 	
 public:
+	/**
+	 * @brief Log an error
+	 * @param Tag The category of the log entry
+	 * @param Text The text to log out
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Unreal Toolbox|Logger")
 	static void Error(const FString Tag, const FString Text);
 
+	/**
+	 * @brief Log a warning
+	 * @param Tag The category of the log entry
+	 * @param Text The text to log out
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Unreal Toolbox|Logger")
 	static void Warning(const FString Tag, const FString Text);
 
+	/**
+	 * @brief Log info
+	 * @param Tag The category of the log entry
+	 * @param Text The text to log out
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Unreal Toolbox|Logger")
 	static void Info(const FString Tag, const FString Text);
 
+	/**
+	 * @brief Log trace information
+	 * @param Tag The category of the log entry
+	 * @param Text The text to log out
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Unreal Toolbox|Logger")
 	static void Trace(const FString Tag, const FString Text);
 
 private:
-	static void LogToFile(ELogType LogType, const FString Tag, const FString Text);
+	/**
+	 * @brief Log information to a file
+	 * @param Type The type of information to log
+	 * @param Tag The category of the log entry
+	 * @param Text The text to log out
+	 */
+	static void LogToFile(ELogType Type, const FString Tag, const FString Text);
+	
+	/**
+	 * @brief Log information to the screen
+	 * @param Color The color used for the log entry
+	 * @param Tag The category of the log entry
+	 * @param Text The text to log out
+	 */
 	static void LogToScreen(const FColor Color, const FString Tag, const FString Text);
-	static FString GetLogTypeText(ELogType LogType);
+	
+	/**
+	 * @brief Convert a log type to a user friendly name
+	 * @param Type The log type to convert
+	 * @return A user friendly name for the log type
+	 */
+	static FString GetLogTypeText(ELogType Type);
 };
